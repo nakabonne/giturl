97c058bc62e417538881181edd5001d1b86d8a87
nakabonne <ryo@nakao.dev>
2020-08-16 11:48:24 +0900

    Add ssh subcommand

--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"os"
 	"runtime"
 
-	flag "github.com/spf13/pflag"
+	"github.com/spf13/cobra"
 
-	"github.com/nakabonne/giturl/pkg/converter"
+	"github.com/nakabonne/giturl/pkg/commands"
 )
 
 var (
-	flagSet = flag.NewFlagSet("giturl", flag.ContinueOnError)
-
-	usage = func() {
-		fmt.Fprintln(os.Stderr, "usage: giturl [<flag> ...] <Git URL> ")
-		flagSet.PrintDefaults()
-	}
-
 	// Automatically populated by goreleaser during build
 	version = "unversioned"
 	commit  = "?"
@@ -27,66 +19,49 @@ var (
 )
 
 type app struct {
-	scheme  string
-	user    string
-	noUser  bool
-	scpLike bool
-	version bool
+	rootCmd *cobra.Command
 	stdout  io.Writer
 	stderr  io.Writer
 }
 
-func main() {
+func newApp(name, desc string) *app {
 	a := &app{
-		scheme: "ssh",
+		rootCmd: &cobra.Command{
+			Use:   name,
+			Short: desc,
+		},
 		stdout: os.Stdout,
 		stderr: os.Stderr,
 	}
-	flagSet.StringVarP(&a.scheme, "scheme", "s", a.scheme, "convert to the given schema: ssh|http|https|git|file")
-	flagSet.StringVar(&a.user, "user", "", "set user")
-	flagSet.BoolVar(&a.noUser, "no-user", false, "prune user from the given URL")
-	flagSet.BoolVarP(&a.scpLike, "scp-like", "S", false, "emit scp-like syntax (available only when --schema=ssh)")
-	flagSet.BoolVarP(&a.version, "version", "v", false, "print the current version")
-	flagSet.Usage = usage
-
-	if err := flagSet.Parse(os.Args[1:]); err != nil {
-		if !errors.Is(err, flag.ErrHelp) {
-			fmt.Fprintln(a.stderr, err)
-		}
-		return
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "print the current version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Fprintf(a.stderr, "version=%s, commit=%s, buildDate=%s, os=%s, arch=%s\n", version, commit, date, runtime.GOOS, runtime.GOARCH)
+		},
 	}
-
-	os.Exit(a.run(flagSet.Args()))
+	a.rootCmd.AddCommand(versionCmd)
+	return a
 }
 
-func (a *app) run(args []string) int {
-	if a.version {
-		fmt.Fprintf(a.stderr, "version=%s, commit=%s, buildDate=%s, os=%s, arch=%s\n", version, commit, date, runtime.GOOS, runtime.GOARCH)
-		return 0
+func (a *app) addCommands(cmds ...*cobra.Command) {
+	for _, cmd := range cmds {
+		a.rootCmd.AddCommand(cmd)
 	}
+}
 
-	if len(args) == 0 {
-		fmt.Fprintln(a.stderr, "No URL given")
-		usage()
-		return 1
-	}
-	if a.scheme == "" {
-		a.scheme = "ssh"
-	}
-	user := a.user
-	if a.noUser {
-		user = ""
-	}
-	opts := converter.Options{
-		User:    &user,
-		ScpLike: a.scpLike,
-	}
-	res, err := converter.Convert(args[0], converter.Scheme(a.scheme), opts)
-	if err != nil {
+func main() {
+	a := newApp("giturl", "A converter for Git URLs")
+
+	a.addCommands(
+		commands.NewSSHCommand(),
+	)
+	if err := a.run(); err != nil {
 		fmt.Fprintln(a.stderr, err)
-		usage()
-		return 1
+		os.Exit(1)
 	}
-	fmt.Fprintf(a.stdout, "%s\n", res)
-	return 0
+}
+
+func (a *app) run() error {
+	return a.rootCmd.Execute()
 }
---
+++ pkg/commands/ssh.go
@@ -0,0 +1,49 @@
+package commands
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/nakabonne/giturl/pkg/converter"
+)
+
+type ssh struct {
+	scpLike bool
+	user    string
+	stdout  io.Writer
+}
+
+func NewSSHCommand() *cobra.Command {
+	r := &ssh{
+		stdout: os.Stdout,
+	}
+	cmd := &cobra.Command{
+		Use:   "ssh",
+		Short: "Convert into ssh syntax",
+		RunE:  r.run,
+	}
+	cmd.Flags().BoolVarP(&r.scpLike, "scp-like", "s", r.scpLike, "emit scp-like syntax")
+	cmd.Flags().StringVar(&r.user, "user", r.user, "override the user")
+
+	return cmd
+}
+func (r *ssh) run(_ *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return fmt.Errorf("no URL given")
+	}
+	opts := converter.Options{
+		ScpLike: r.scpLike,
+	}
+	if r.user != "" {
+		opts.User = &r.user
+	}
+	res, err := converter.Convert(args[0], converter.SchemeSSH, opts)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(r.stdout, res)
+	return nil
+}
